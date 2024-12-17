e665c5efe9d80c570bda2b3ed9a83d0895cc3090
Soumesh Nayak <soumeshnayak2210@gmail.com>
2024-12-17 22:01:40 +0530

    Added List

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,9 +1,24 @@
+import Image from 'next/image'
 import React from 'react'
-
+import Filter from "@/components/Filter"
+import ProductList from '@/components/ProductList'
 const ListPage = () => {
   return (
-    <div>
-      List Page
+    <div className='px-4 md:px-8 lg:px-16 xl:32 2xl:px-64'>
+      <div className='hidden bg-pink-50 px-4 sm:flex justify-between h-64'>
+        <div className='w-2/3 flex flex-col items-center justify-center gap-8'>
+        <h1 className='text-4xl font-semibold leading-[48px] text-gray-700'>Grap Up to 50% off on<br/> selected Products</h1>
+        <button className='rounded-3xl bg-lama text-white w-max py-3 px-5 text-sm'>Buy Now</button>
+        </div>
+        <div className='relative w-1/3'>
+          <Image src='/woman.png' alt='' fill className='object-contain'/>
+        </div>
+      </div>
+      {/* Filter */}
+      <Filter/>
+      {/* Product */}
+      <h1 className='mt-12 text-xl font-semibold'>Shoes for you</h1>
+      <ProductList/>
     </div>
   )
 }
